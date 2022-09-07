c2a39417703dad214ccbc94289bf14540ace85f6
Jaichawla1234 <jai05720803120@bpitindia.com>
2022-09-07 10:37:37 +0530

    commit20

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/messenger',authRouter);
 app.use('/api/messenger',messengerRoute);
 
 app.use(cors({
-  origin : [ 'https://chat-application6912.netlify.app' , 'https://chat-app-it-b-c.herokuapp.com/' ],
+  origin : [ 'https://chat-application6912.netlify.app' , 'https://chat-app-it-b-c.herokuapp.com' ],
   methods:["GET" , "POST" , "PUT", "DELETE"],
   credentials: true
 }));
