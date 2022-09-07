4016d2ec41e10e56d82e19f66e0dc9b0b7febc5a
Jaichawla1234 <jai05720803120@bpitindia.com>
2022-09-07 10:41:53 +0530

    commit21

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const {userRegister,userLogin,userLogout} = require('../controller/authController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 
-router.post('/user-login',userLogin);
+router.get('/user-login',userLogin);
 router.post('/user-register',userRegister);
 router.post('/user-logout',authMiddleware,userLogout);
 
